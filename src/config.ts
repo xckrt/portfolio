543d7b1a59f5fef1018a907fe9d4d01e96fbbb75
xckrt <graf196969@gmail.com>
2025-12-28 16:14:13 +0000

    Reinstall Theme

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'xckrt | Android | .NET Dev';
+export const SITE_DESCRIPTION = 'Portfolio of Alexey Kalinin - C#, WPF, ASP.NET, Android Developer';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
